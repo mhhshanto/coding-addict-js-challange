0c4e4737c3f4f51f8965008724107b7becf7da2a
shanto <hhassibul@gmail.com>
2022-06-12 20:31:03 +0600

    euie

--- Return3Times.js
+++ Return3Times.js
@@ -1,8 +1,11 @@
-function ret(a,b){
-  return function(c,d){
-    return function(e,f){
-      return a*c*e+b*d*f
-    }
-  }
-}
+// function ret(a,b){
+//   return function(c,d){
+//     return function(e,f){
+//       return a*c*e+b*d*f
+//     }
+//   }
+// }
+// console.log(ret(1,2)(3,4)(5,6));
+// console.log(6+7);
+ret = (a,b) =>(c,d) =>(e,f) => a*c*e+b*d*f
 console.log(ret(1,2)(3,4)(5,6));
